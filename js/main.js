6778098addb0faecb39c89d55c6f8f13b4bb12a7
Trevor Hartman <trevorhartman@gmail.com>
2010-04-11 23:39:21 -0700

    Added links to insert another row. Basic functionality working for adding paragraphs only.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,49 @@
 $(function(){
 	
-	$("input.text, textarea").keyup(function() {
+	var currentRowId = 3; // 1 and 2 are initialized by default
+	sh_highlightDocument();
+	init_keyup_listener();
+	
+	function init_keyup_listener() {
+		$("input.text, textarea").keyup( input_keyup );
+	}
+	
+	function input_keyup()
+	{
+		console.log("hi");
 		$(this).closest(".tekisuto").next('.code').find('.code-content').text(  $(this).val() );
+	}
+	
+	
+	var tag_map = {
+		paragraph: "p",
+		headline: "h1"
+	};
+	
+	$("#insert_menu a").click(function(){
+		$this = $(this);
+		
+		var tag = tag_map[ $this.text().toLowerCase() ];
+		
+		console.log( tag + " : " + $this.text() );
+		
+		buildItem( tag );
+		
+		return false;
 	});
 	
 	
-	sh_highlightDocument();
+	
+	
+	function buildItem( tag )
+	{
+		$template = $("#row_template").clone();
+		
+		$("table").append($template.show());
+		
+		init_keyup_listener();
+	}
+	
+
 	
 });
